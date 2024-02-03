de8ee301f00478d34c51531ae998bc15b7d9536b
Andrew Miller <socrates1024@gmail.com>
2024-02-02 20:45:44 -0600

    Now takes cmd args. Processes stdin/stdout in addition to app server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 use clap::Parser;
-use std::io;
-
-use acme_lib::{create_p256_key,create_rsa_key};
+use acme_lib::{Result,create_rsa_key};
 use suave_andromeda_revm::StatefulExecutor;
-use openssl::x509::{X509Req, X509ReqBuilder, X509};
+use openssl::x509::{X509Req, X509ReqBuilder};
 use openssl::x509::extension::SubjectAlternativeName;
 use openssl::pkey::{self, PKey};
 use openssl::hash::MessageDigest;
 use openssl::stack::Stack;
-use acme_lib::Result;
 
-use rouille::{Request,Response,Server};
 
 pub fn create_csr(pkey: &PKey<pkey::Private>) -> Result<X509Req> {
     //
@@ -56,37 +52,77 @@ struct Cli {
     rpc: String,
     #[arg(short, long, default_value_t = false)]
     trace: bool,
+    #[arg(short, long, default_value_t = false)]
+    bootstrap: bool,
 }
+
+use warp::Filter;
+use std::{sync::{Arc}};
+use tokio::{io::{AsyncBufReadExt},sync::Mutex};
+use suave_andromeda_revm::{andromeda_precompiles, sgx_precompiles};
+use futures::future::join_all;
+use async_std::{task};
 
 #[tokio::main]
 async fn main() {
     let cli_args = Cli::parse();
     let mut service = StatefulExecutor::new_with_rpc(cli_args.rpc.clone());
 
-    let pkey = create_rsa_key(2048);
-    let csr = create_csr(&pkey);
-    println!("{}", String::from_utf8(pkey.rsa().unwrap().private_key_to_pem().unwrap()).unwrap());
-    println!("{}", String::from_utf8(csr.unwrap().to_pem().unwrap()).unwrap());
+    /* For bootstrapping, 
+       we will inject the CERT and CSR into a special place associated with 
+       the contract. */
+    if cli_args.bootstrap {
+	let pkey = create_rsa_key(2048);
+	let csr = create_csr(&pkey);
+	println!("{}", String::from_utf8(pkey.rsa().unwrap().private_key_to_pem().unwrap()).unwrap());
+	println!("{}", String::from_utf8(csr.unwrap().to_pem().unwrap()).unwrap());
+    }
 
-    Server::new_ssl("0.0.0.0:5001",
-		    move |request| {
-/*
-			service
-			    .execute_command(command, cli_args.trace)
-			    .match
-			{
-			    Ok(res) => println!("{:?}", res),
-			    Err(e) => println!("{:?}", e),
-			}
-*/			
-			Response::text("hello world")
-		    },
-		    include_bytes!("../ssl-cert.pem").to_vec(),
-		    include_bytes!("../ssl-key.pem").to_vec(),
-    )
-	.expect("Failed to start server")
-        .run();    
-    
+    let myservice = Arc::new(Mutex::new(service));
+    let myservice2 = myservice.clone();
+
+    // Match any request and return hello world!
+    let routes = warp::any().then(move || {
+	let myservice = myservice2.clone();
+	async move {
+	    let mut service = myservice.lock().await;
+	    match service
+		.execute_command("advance", false)
+		.await
+	    {
+		Ok(res) => format!("{:?}", res),
+		Err(e) => format!("{:?}", e),
+	    }
+	}
+    });
+
+    let warpserver = task::spawn(warp::serve(routes)
+        .tls()
+	.cert(include_bytes!("../ssl-cert.pem").to_vec())
+	.key(include_bytes!("../ssl-key.pem").to_vec())
+        .run(([0, 0, 0, 0], 5001)));
+
+    let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
+    let task = task::spawn(async move {
+	loop {
+	    let mut buffer = Vec::new();
+	    let _fut = reader.read_until(b'\n', &mut buffer).await;
+	    let mut service = myservice.lock().await;	
+	    match service
+		.execute_command(&String::from_utf8(buffer)
+				 .expect("utf8 failed")
+				 .strip_suffix("\n")
+				 .expect("newlin failed"), cli_args.trace)
+		.await
+	    {
+		Ok(res) => println!("{:?}", res),
+		Err(e) => println!("{:?}", e),
+	    }
+	}
+    });
+			   
+    join_all(vec![warpserver, task]).await;
+
     /*
     Usage plan:
     1. Advance the chain
@@ -96,45 +132,6 @@ async fn main() {
     3. Support onboarding
     // If not bootstrapped:
     */
-    
-    // TODO: probably doesnt work due to async
-    loop {
-        let mut input_buf = String::new();
-        io::stdin().read_line(&mut input_buf).expect("");
 
-        // We support two commands: advance <block number|latest|empty(latest)> and execute <TxEnv json>
-	let input = input_buf.trim();
-
-        let (command, args) = match input.split_once(' ') {
-            Some((command, args)) => (command, Some(args)),
-            None => (input, None),
-        };
-
-        match command {
-	    "bootstrap" => {
-		// Create and store a TLS certificate.
-	    }
-
-
-	    "load" => {
-		// 
-	    }
-	    
-
-            "serve" => {
-		// Start a new thread to serve a TLS endpoint
-	    }
-
-
-	    // Fallback to the "Stateful Exec" service.
-	    _ => match service
-		.execute_command(command, cli_args.trace)
-		.await
-            {
-		Ok(res) => println!("{:?}", res),
-		Err(e) => println!("{:?}", e),
-            }
-	    
-	}
-    }
+    // We support two commands: advance <block number|latest|empty(latest)> and execute <TxEnv json>
 }
